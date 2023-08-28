51286eb5408cec3cb5b33f5a2b38a9e1554146c1
pushparaj-c247 <pushparaj.m@chapter247.com>
2023-08-28 10:08:16 +0530

    testing the git branch

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,5 @@
 import passport from "../config/passport"
 import errorHandler from "./errorHandler"
 import errorLast from "./errorLast"
-
+// this my errorhandler
 export { passport, errorHandler, errorLast }
